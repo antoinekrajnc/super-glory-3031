70193660df85fccbdcd31646a9f107bec23386cf
antoinekrajnc <antoinekrajnc@gmail.com>
2017-06-24 18:28:41 -0700

    final update

--- index.php
+++ index.php
@@ -32,69 +32,13 @@
     <div class="fixed-bottom d-flex flex-row align-items-center justify-content-around mb-3">
       <div class="sign-up d-flex justify-content-end mt-4">
         <!-- Button trigger modal -->
-        <a class="sign-up-btn" data-toggle="modal" data-target="#exampleModal">
+        <a class="sign-up-btn" href="https://superglorygame.wufoo.com/forms/beta-sign-up/">
           sign up beta
         </a>
       </div>
       <div class="social-media d-flex flex-row align-items-center justify-content-start mr-5 mt-4">
-        <a href="#"><img class="social-media-logo" src="assets/websitetwitterlogo.png" alt="twitter"></a>
-        <a href="#"><img class="social-media-logo" src="assets/websitefblogo.png" alt="facebook"></a>
-        <a href="#"><img class="social-media-logo" src="assets/websiteinstalogo.png" alt="instagram"></a>
-      </div>
-    </div>
-
-    <!-- Modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Sign up to our newsletter</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <form>
-            <div class="modal-body">
-              <!-- Begin MailChimp Signup Form -->
-              <link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
-              <style type="text/css">
-              	#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
-              	/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
-              	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
-              </style>
-              <div id="mc_embed_signup">
-              <form action="//facebook.us16.list-manage.com/subscribe/post?u=6281ed0be2eac9cfa1fb86dfa&amp;id=80eb5b09da" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-                  <div id="mc_embed_signup_scroll">
-              	<h2>Subscribe to our mailing list</h2>
-              <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
-              <div class="mc-field-group">
-              	<label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
-              </label>
-              	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
-              </div>
-              <div class="mc-field-group">
-              	<label for="mce-FNAME">First Name </label>
-              	<input type="text" value="" name="FNAME" class="" id="mce-FNAME">
-              </div>
-              <div class="mc-field-group">
-              	<label for="mce-LNAME">Last Name </label>
-              	<input type="text" value="" name="LNAME" class="" id="mce-LNAME">
-              </div>
-              	<div id="mce-responses" class="clear">
-              		<div class="response" id="mce-error-response" style="display:none"></div>
-              		<div class="response" id="mce-success-response" style="display:none"></div>
-              	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                  <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_6281ed0be2eac9cfa1fb86dfa_80eb5b09da" tabindex="-1" value=""></div>
-                  <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-                  </div>
-              </form>
-              </div>
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-              <button type="submit" class="btn btn-primary">Submit</button>
-            </div>
-        </div>
+        <a href="https://twitter.com/superglorygame"><img class="social-media-logo" src="assets/websitetwitterlogo.png" alt="twitter"></a>
+        <a href="https://facebook.com/superglorygame"><img class="social-media-logo" src="assets/websitefblogo.png" alt="facebook"></a>
       </div>
     </div>
   </body>
