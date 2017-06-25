8a75056e1bab1edc750cebe32039a52aac7167a2
antoinekrajnc <antoinekrajnc@gmail.com>
2017-06-24 17:56:23 -0700

    update site + test form

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
       </div>
         <img class="logo ml-5 mr-5" src="assets/websitelogo2.png" alt="super-glory-3031-log">
       <div class="d-flex flex-column">
-        <a class="navbar-link d-flex justify-content-end ml-5" href="#">Blog</a>
+        <a class="navbar-link d-flex justify-content-end ml-5" href="http://superglorygame.tumblr.com">Blog</a>
       </div>
     </div>
     <div class="d-flex justify-content-center">
@@ -32,7 +32,7 @@
     <div class="fixed-bottom d-flex flex-row align-items-center justify-content-around mb-3">
       <div class="sign-up d-flex justify-content-end mt-4">
         <!-- Button trigger modal -->
-        <a class="sign-up-btn navlink" data-toggle="modal" data-target="#exampleModal">
+        <a class="sign-up-btn" data-toggle="modal" data-target="#exampleModal">
           sign up beta
         </a>
       </div>
@@ -55,13 +55,47 @@
           </div>
           <form>
             <div class="modal-body">
-                <input class="email-input" type="email" placeholder="Email">
+              <!-- Begin MailChimp Signup Form -->
+              <link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
+              <style type="text/css">
+              	#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+              	/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+              	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+              </style>
+              <div id="mc_embed_signup">
+              <form action="//facebook.us16.list-manage.com/subscribe/post?u=6281ed0be2eac9cfa1fb86dfa&amp;id=80eb5b09da" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                  <div id="mc_embed_signup_scroll">
+              	<h2>Subscribe to our mailing list</h2>
+              <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+              <div class="mc-field-group">
+              	<label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
+              </label>
+              	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+              </div>
+              <div class="mc-field-group">
+              	<label for="mce-FNAME">First Name </label>
+              	<input type="text" value="" name="FNAME" class="" id="mce-FNAME">
+              </div>
+              <div class="mc-field-group">
+              	<label for="mce-LNAME">Last Name </label>
+              	<input type="text" value="" name="LNAME" class="" id="mce-LNAME">
+              </div>
+              	<div id="mce-responses" class="clear">
+              		<div class="response" id="mce-error-response" style="display:none"></div>
+              		<div class="response" id="mce-success-response" style="display:none"></div>
+              	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                  <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_6281ed0be2eac9cfa1fb86dfa_80eb5b09da" tabindex="-1" value=""></div>
+                  <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+                  </div>
+              </form>
+              </div>
+              <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+              <!--End mc_embed_signup-->
             </div>
             <div class="modal-footer">
               <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
               <button type="submit" class="btn btn-primary">Submit</button>
             </div>
-          </form>
         </div>
       </div>
     </div>
